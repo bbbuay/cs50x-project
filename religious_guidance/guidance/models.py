20906e43575445faea45686b0c42ae14c245f7dd
buay <bbuaykkem@gmail.com>
2024-10-30 23:47:14 +1100

    add Guidance model

--- religious_guidance/guidance/models.py
+++ religious_guidance/guidance/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from user.models import UserProfile
+
+class Religion(models.TextChoices):
+    Buddhism = 'B', 'Buddhism'
+    Christianity = 'C', 'Christianity'
 
 # Create your models here.
+class Guidance(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    image = models.ImageField(upload_to="guidance_images/")
+    religion = models.CharField(max_length=1, choices=Religion.choices)
+    favorite_users = models.ManyToManyField(UserProfile, related_name="favorite_guidances")
