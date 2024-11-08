d43b03d028062d301a2513226bc6b85ed5c7cb4e
buay <bbuaykkem@gmail.com>
2024-11-08 16:07:14 +0700

    update name of the urls

--- religious_guidance/guidance/urls.py
+++ religious_guidance/guidance/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import RandomGuidanceDetail, LikeGuidanceView, UnlikeGuidanceView
 
 urlpatterns = [
-    path('guidances/random/', RandomGuidanceDetail.as_view(), name='random-guidance-detail'),
-    path('guidances/<int:id>/like/', LikeGuidanceView.as_view(), name='like-guidance'),
-    path('guidances/<int:id>/unlike/', UnlikeGuidanceView.as_view(), name='unlike-guidance'),
+    path('guidances/random/', RandomGuidanceDetail.as_view(), name='random_guidance_detail'),
+    path('guidances/<int:id>/like/', LikeGuidanceView.as_view(), name='like_guidance'),
+    path('guidances/<int:id>/unlike/', UnlikeGuidanceView.as_view(), name='unlike_guidance'),
 ]
--- religious_guidance/user/urls.py
+++ religious_guidance/user/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import RegisterUserView, UserProfileView
 
 urlpatterns = [
-    path('register/', RegisterUserView.as_view(), name='register-user'),
-    path('profile/', UserProfileView.as_view(), name='get_profile')
+    path('register/', RegisterUserView.as_view(), name='register_user'),
+    path('profile/', UserProfileView.as_view(), name='get_user_profile'),
 ]
