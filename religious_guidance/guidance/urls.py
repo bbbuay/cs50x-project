a1fe0d9397f207fcce678f42bcd61572406bd14b
buay <bbuaykkem@gmail.com>
2024-11-08 11:47:19 +0700

    add like guidance api

--- religious_guidance/guidance/urls.py
+++ religious_guidance/guidance/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import RandomGuidanceDetail
+from .views import RandomGuidanceDetail, LikeGuidanceView
 
 urlpatterns = [
-    path('guidances/random/', RandomGuidanceDetail.as_view(), name='random-guidance-detail')
+    path('guidances/random/', RandomGuidanceDetail.as_view(), name='random-guidance-detail'),
+    path('guidances/<int:id>/like/', LikeGuidanceView.as_view(), name='like-guidance')
 ]
--- religious_guidance/guidance/views.py
+++ religious_guidance/guidance/views.py
@@ -4,6 +4,8 @@ from random import randint
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
 
 class RandomGuidanceDetail(generics.RetrieveAPIView):
     queryset = Guidance.objects.all()
@@ -26,3 +28,21 @@ class RandomGuidanceDetail(generics.RetrieveAPIView):
         random_index = randint(0, queryset.count()-1)
 
         return queryset[random_index]
+    
+
+class LikeGuidanceView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, id):
+        profile = request.user.profile
+
+        try:
+            guidance = Guidance.objects.get(id=id)
+        except Guidance.DoesNotExist:
+            return Response({"error": "Guidance does not exist of the given ID"}, status=status.HTTP_404_NOT_FOUND) 
+        
+        if profile in guidance.favorite_users.all():
+            return Response({"error": "The user already like this guidance"}, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            profile.favorite_guidances.add(guidance)
+            return Response({"message": "User successfully like this guidance."}, status=status.HTTP_200_OK)
