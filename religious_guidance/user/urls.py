ccb3d623ae051b86790a67cdfaab3c6914b6d655
buay <bbuaykkem@gmail.com>
2024-11-12 21:17:04 +0700

    fix urls.py

--- religious_guidance/user/urls.py
+++ religious_guidance/user/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import RegisterUserView, UserProfileView, FavoriteGuidanceView, login_view, register_view, profile_view
 
 urlpatterns = [
-    path('login', login_view, name='login'),
-    path('register', register_view, name='register'),
-    path('profile', profile_view, name='profile'),
+    path('login/', login_view, name='login'),
+    path('register/', register_view, name='register'),
+    path('profile/', profile_view, name='profile'),
 
     # API endpoints
     path('api/register/', RegisterUserView.as_view(), name='register_user'),
